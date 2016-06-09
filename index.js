9511fd3f76daf97be8208854b05a492aba982622
egig <egigundari@gmail.com>
2016-06-10 02:41:57 +0700

    init

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+
+  fs.stat(__dirname+"/storage/playlist.txt", function(err, stat) {
+    if(err == null) {
+        console.log('File exists');
+        fs.unlink(__dirname+"/storage/playlist.txt");
+    } else if(err.code == 'ENOENT') {
+        // fs.writeFile('log.txt', 'Some log\n');
+        return;
+    } else {
+        console.log('Some other error: ', err.code);
+    }
+});
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/slave', function(req, res){
+  res.sendFile(__dirname + '/slave.html');
+});
+
+app.get("/init-playlist.html", function(req, res) {
+
+  fs.readFile(__dirname + "/storage/playlist.txt", 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    res.end(data);
+  });
+});
+
+io.on('connection', function(socket){
+
+  console.log('a user connected: ', socket.id);
+
+  socket.on('disconnect', function(){
+   console.log('user disconnected');
+  });
+
+  socket.on('playlist.updated', function(t){
+     io.emit('playlist.updated', t);
+
+    // save playlist
+    fs.appendFile(__dirname + "/storage/playlist.txt", t+"\r\n", function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+  });
+
+  socket.on('track.played', function(link, host){
+     io.emit('track.played', link, host);
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ public/main.js
@@ -0,0 +1,75 @@
+(function($) {
+
+  var socket = io();
+  var playlist = $('#playlist');
+  var audio = $('audio');
+
+  $(track_chooser).change(function() {
+    for (f of track_chooser.files) {
+      var url = URL.createObjectURL(f);
+      var h = "<li><a class='track' href="+url+">"+f.name+"</a></li>";
+      socket.emit('playlist.updated', h);
+    }
+  });
+
+  socket.on('playlist.updated', function(h){
+    playlist.append(h);
+  });
+
+  socket.on('track.played', function(link, host){
+
+    console.log(link);
+    console.log(socket.id);
+    //if(socket.id == host) {
+      run(link, audio[0]);
+    //}
+
+  });
+
+  var playlist;
+  var tracks;
+  var current;
+
+  init();
+  function init(){
+      current = 0;
+      playlist = $('#playlist');
+      tracks = playlist.find('li a.track');
+
+      var len = tracks.length - 1;
+      audio[0].volume = .10;
+      //audio[0].play();
+
+      $(document).on('click', 'a.track', function(e){
+          e.preventDefault();
+          var a = $(this);
+          current = a.parent().index();
+          // run(link, audio[0]);
+          var link = a.attr('href');
+
+          socket.emit('track.played', link, socket.id);
+      });
+
+      audio[0].addEventListener('ended',function(e){
+          current++;
+          if(current == len){
+              current = 0;
+              link = playlist.find('a.track')[0];
+          }else{
+              link = playlist.find('a.track')[current];
+          }
+
+           run($(link).attr("href") , audio[0]);
+          //socket.emit('track.played', link);
+      });
+  }
+
+  function run(link, player){
+      player.src = link
+      audio[0].load();
+      audio[0].play();
+  }
+
+  playlist.load("/init-playlist.html");
+
+})(jQuery);

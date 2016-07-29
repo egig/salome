8fb68fc234ca629813d3ffd628b75215b2d7941a
egig <egigundari@gmail.com>
2016-07-29 08:18:21 +0700

    remove unused model

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-          User.hasMany(models.Task)
+          //..
       }
     }
   });

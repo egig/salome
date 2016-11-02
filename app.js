8a476b645cd3b042945acfcd35b30d9ff3aaea22
egig <egigundari@gmail.com>
2016-11-02 08:20:45 +0700

    add basic auth

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var EE = require('express-extended');
 var express = EE.express;
 var app = EE();
+var auth = require('basic-auth')
+var config = require('./config');
 
 app.use('/bower_components', express.static(__dirname+'/bower_components'));
 
-var config = require('./config');
+/**
+ * Simple auth middlewares
+ */
+app.use(function (req, res, next) {
+  var credentials = auth(req)
+
+  if (!credentials || credentials.name !== config.salome.username || credentials.pass !== config.salome.password) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="restricted"')
+    res.end('Access denied')
+  } else {
+    next()
+  }
+})
+
 app.load(__dirname, config);
 
 module.exports = app;
